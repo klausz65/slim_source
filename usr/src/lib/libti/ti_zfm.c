0ee8d0c7ec4175ff634cc70d2c111852e8d6e2f5
Carsten Grzemba <cgrzemba@opencsw.org>
2025-09-11 13:27:09 +0200

    gui-install: use cXtXd0 for whole disk install, fix

--- usr/src/lib/libti/ti_zfm.c
+++ usr/src/lib/libti/ti_zfm.c
@@ -416,7 +416,7 @@ zfm_create_pool(nvlist_t *attrs)
 	    "zfs: ZFS pool <%s> will be created on slice <%s>\n",
 	    zfs_pool_name, zfs_device);
 
-	if ((strlen(zfs_device) - strcspn(zfs_device, "d0")) == 2) {
+	if (strlen(strstr(zfs_device, "d0")) == 2) {
 		(void) snprintf(cmd, sizeof (cmd),
 		    "/usr/sbin/zpool create -f -B %s %s",
 		    zfs_pool_name, zfs_device);
